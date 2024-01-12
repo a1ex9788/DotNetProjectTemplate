345789696984f67d81bb5ae1f008227ffde27d68
a1ex9788 <alejandrocarrion1999@gmail.com>
2024-01-12 20:29:27 +0100

    Feat: Crear proyectos.

---
+++ Code/Project/Program.cs
@@ -0,0 +1,39 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+
+namespace SystemMonitor
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                using CommandLineApplication commandLineApplication = new CommandLineApplication()
+                {
+                    Name = "project",
+                };
+
+                commandLineApplication.HelpOption();
+
+                DefineCommand(commandLineApplication);
+
+                return commandLineApplication.Execute(args);
+            }
+            catch
+            {
+                Console.Error.WriteLine("An unexpected error was produced.");
+
+                return -1;
+            }
+        }
+
+        private static void DefineCommand(CommandLineApplication commandLineApplication)
+        {
+            commandLineApplication.OnExecute(() =>
+            {
+                Console.WriteLine("This is a project template.");
+            });
+        }
+    }
+}
---
+++ Code/Project/Tests/UnitTests/ProjectTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project.Tests.UnitTests
+{
+    [TestClass]
+    public class ProjectTests
+    {
+        [TestMethod]
+        public void Project_Test_ReturnsExpectedResult()
+        {
+            // Arrange.
+            int a = 5;
+            int b = 0;
+
+            // Act.
+            int c = a - b;
+
+            // Assert.
+            c.Should().Be(5);
+        }
+    }
+}
